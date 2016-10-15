99012f88d0ca305481de2f9f426baf8ce664cda6
kliprift <eddie@worldone.com.my>
2016-10-15 18:52:40 +0800

    edit update and destroy

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -21,6 +21,22 @@ class PicsController < ApplicationController
     render 'new'
   end
 
+  def edit
+  end
+
+  def update
+    if @pic.update(pic_params)
+      redirect_to @pic, notice: "Pics has been updated"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pic.destroy
+    redirect_to root_path
+  end
+
  end
 
  private
